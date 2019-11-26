7e0bb9b93232bd5b43568491abda0b06ad442b99
fawwazmayda <maydafawwaz@gmail.com>
2019-11-27 03:42:02 +0700

    Customer Schema

---
+++ Schemas/customerSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://m001-student:abangbola@sandbox-wtclz.mongodb.net/test?retryWrites=true&w=majority",
+{useNewUrlParser:true, dbName:"jahit"})
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log("Sambung")
+});
+
+
+let customerSchema = new mongoose.Schema({
+    _id : Number,
+    username : String,
+    pwd : String,
+    nohp : String
+})
+
+let edu = mongoose.model('customer',customerSchema,'customer')
+console.log("customer Terbuat")
+module.exports = mongoose.model('customer')
