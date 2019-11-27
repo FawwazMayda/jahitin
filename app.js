f01daf928878df33c3675a0564904a8cee6f7e78
fawwazmayda <maydafawwaz@gmail.com>
2019-11-27 12:26:22 +0700

    Add customer and tailor controller

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app =express()
 const cors = require('cors')
 const orderController= require('./orderController')
 const productController = require('./productController')
+const customerController = require('./customerController')
+const tailorController = require('./tailorController')
 
 app.use(cors())
 app.use('/api/orderController',orderController)
 app.use('/api/productController',productController)
+app.use('/api/customerController',customerController)
+app.use('/api/tailorController',tailorController)
 
 module.exports = app
